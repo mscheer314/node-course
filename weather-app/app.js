459511c71576a921b3c0cb64219d663fae089721
scheerm <mark.scheer@wwt.com>
2023-07-18 20:32:42 -0500

    add mapbox api call

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,14 @@
 const config = require('./config')
 const request = require('postman-request')
 
-const url = `http://api.weatherstack.com/current?access_key=${config.apiKey}&query=37.8267,-122.4233&units=f`
+const location = 'Chicago'
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${config.apiKey}&query=37.8267,-122.4233&units=f`
+const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${config.mapboxApiKey}`
 
-request({ url: url, json: true}, (error, response) => {
-  console.log(`It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+// request({ url: weatherUrl, json: true}, (error, response) => {
+//   console.log(`It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+// })
+
+request({url: mapboxUrl, json: true}, (error, response) => {
+  console.log(response)
 })
